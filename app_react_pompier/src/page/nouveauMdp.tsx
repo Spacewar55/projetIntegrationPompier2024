9dc7cbae096e3f9001eb4b79fe2d65902664d20c
Alex <alexds2000@outlook.fr>
2024-03-21 17:09:36 -0400

    nettoyage code

--- app_react_pompier/src/page/nouveauMdp.tsx
+++ app_react_pompier/src/page/nouveauMdp.tsx
@@ -6,7 +6,6 @@ import Typography from '@mui/material/Typography';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Container from '@mui/material/Container';
 
-// TODO remove, this demo shouldn't need to reset the theme.
 const defaultTheme = createTheme();
 
 export default function nouveauMDP() {
